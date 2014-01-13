dc57d5038574d6757cdb6ce96db4a21c8743dc12
your name <madera.eric@gmail.com>
2014-01-13 09:38:27 -0800

    loops, operators

--- app/src/modules/pages/js/js-loops/JsLoopsCtrl.js
+++ app/src/modules/pages/js/js-loops/JsLoopsCtrl.js
@@ -4,6 +4,89 @@
 'use strict';
 
 angular.module('myApp').controller('JsLoopsCtrl', ['$scope',
-function($scope) {
+function($scope)
+{
+	//Javascript supports several types of loops.
+	//The 'for' loop is by far the most important for web developers, as it is well-suited for about 99% of your looping needs.
 	
+	//Here is an example.
+	
+	var ii;
+	var my_array = [];
+	
+	for(ii = 0; ii < 5; ii++)
+	{
+		my_array.push(ii);
+	}
+	//When the loop is complete, my_array will be [0, 1, 2, 3, 4]
+	
+	//The loop works like this:
+	// 1) ii = 0; (the code up to the first semicolon) is executed. This is this loop's initialization step; the code placed here is run exactly once, before the loop begins.
+	// 2) ii < 5; (the code up to the second semicolon) is executed. If the result is false, the loop is over. If true, proceed to step 3.
+	// 3) The code inside the { } (in this case, my_array.push(ii);) is executed.
+	// 4) ii++; (the code after the second semicolon) is executed.
+	// 5) Go to step 2.
+	
+	//A common use of 'for' loops is to iterate over every entry in an array, like this:
+	
+	for(ii = 0; ii < my_array.length; ii++)
+	{
+		if(ii !== 1)
+		{
+			my_array[ii] += 5;	//Add 5 to every entry (except the second) in the array
+		}
+	}
+	//When the loop is complete, my_array will be [5, 1, 7, 8, 9]
+	
+	//There is a second type of 'for' loop, used with objects. The syntax is a bit simpler.
+	var my_object = {'prop1': 1, 'prop2': 2; 'prop3': 40 };
+	
+	var xx;
+	for(xx in my_object)
+	{
+		my_object[xx] += 2;
+	}
+	//When the loop is complete, my_object will be {'prop1': 3, 'prop2': 4; 'prop3': 42 }
+	
+	//This loop is run once for every property in my_object. The variable xx will hold the current property name ('prop1', 'prop2', or 'prop3', in this case.)
+	//Looping over objects does not have a set order. 'prop2' may be the first, second, or third value set to xx here.
+	
+	//The other type of loop you should be aware of is the 'while' loop.
+	//Every (non-object) 'for' loop can be written as a 'while' loop, and every 'while' loop can be written as a 'for' loop.
+	
+	//Here's the same 'for' loop as before, written as a while loop
+	ii = 0;
+	while(ii < my_array.length)
+	{
+		if(ii !== 1)
+		{
+			my_array[ii] += 5;
+		}
+		
+		ii++;
+	}
+	//The loop is simple:
+	// 1) Run the code in parentheses. If false, stop looping. If true, go to step 2.
+	// 2) Run the code in { }.
+	// 3) Go to step 1.
+	
+	//There is also a 'do-while' loop:
+	ii = 0;
+	do
+	{
+		if(ii !== 1)
+		{
+			my_array[ii] += 5;
+		}
+		
+		ii++;
+	} while(ii < my_array.length);
+	
+	//The only difference between 'do-while' and 'while' is that 'do-while' starts at the 'while' loop's step 2,
+	//meaning that the code in { } will always be run at least once.
+	
+	
+	//Beware of infinite loops! Always make sure that your loop gets closer to finishing with each step.
+	//If you forget to increment ii, for example, or if you make a mistake with the stop condition,
+	//your loop will never complete, and your web page will never finish loading!
 }]);
--- app/src/modules/pages/js/js-operators/JsOperatorsCtrl.js
+++ app/src/modules/pages/js/js-operators/JsOperatorsCtrl.js
@@ -4,6 +4,57 @@
 'use strict';
 
 angular.module('myApp').controller('JsOperatorsCtrl', ['$scope',
-function($scope) {
+function($scope)
+{
+	//Arithmetic operators include +, -, *, /, %; you can find more about these in the Math lesson, but they're pretty self-explanatory.
 	
+	//Javascript also supports increment and decrement operators, ++ and --, for numbers, as well as shorthand arithmetic operators +=, -=, *=, /=, %=
+	var xx = 5;
+	var yy = 3;
+	
+	xx++;		//Equivalent to xx = xx + 1;
+	yy--;		//Equivalent to yy = yy - 1;
+	
+	xx += 5;	//Equivalent to xx = xx + 5;
+	xx -= 2;	//Equivalent to xx = xx - 2;
+	yy *= 6;	//Equivalent to yy = yy * 6;
+	yy /= 3;	//Equivalent to yy = yy / 3;
+	xx %= 2;	//Equivalent to xx = xx % 2;
+	
+	//The + operator can additionally be used on strings, where it works as concatenation:
+	
+	var str1 = 'foo' + 'bar';	//str1 == 'foobar';
+	var str2 = 'Hello ' + str1;	//str2 == 'Hello foobar'
+	
+	//If you add a string and a number, you'll get a string
+	var str3 = str1 + 1234;		//str3 == 'foobar1234';
+	
+	//The logical operators && (and), || (or), and ! (not) are covered in the Logic lesson.
+
+	//You've already seen most of the comparison operators: ==, ===, !=, !==, >, <, >=, <=
+	//All of these operators compare two entities and produce true or false.
+	
+	//== and === check if two things are the same. === is stronger; it checks value as well as type, and should be used when comparing directly to numbers, booleans, and undefined
+	
+	xx = 5;
+	//xx == '5';	//true
+	//xx === '5';	//false
+	//xx === 5;		//true
+	
+	// != and !== are simply the negation of == and ===
+	//xx != '5';	//false
+	//xx !== '5';	//true
+	//xx !== 5;		//false
+	
+	//<, <=, >, >= are less than, less than or equal to, greater than, and greater than or equal to.
+	//They work exactly as you expect with numbers.
+	//xx < 5;		//false
+	//xx <= 5;		//true
+	//xx > 2;		//true
+	//xx >= 6;		//false
+	
+	//These operators can also be used on strings. They compare alphabetically in this case, with 'a' < 'b' < 'c', etc.
+	
+	//'foo' < 'b';	//false
+	//'xyz' > 'ab';	//true
 }]);
