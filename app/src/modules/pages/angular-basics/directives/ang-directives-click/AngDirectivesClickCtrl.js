0290f079bb362c2f795b055bce8b159e140ae149
your name <madera.eric@gmail.com>
2014-02-02 11:45:07 -0800

    ng-click, ng-show

--- app/src/modules/pages/angular-basics/directives/ang-directives-click/AngDirectivesClickCtrl.js
+++ app/src/modules/pages/angular-basics/directives/ang-directives-click/AngDirectivesClickCtrl.js
@@ -4,6 +4,23 @@
 'use strict';
 
 angular.module('myApp').controller('AngDirectivesClickCtrl', ['$scope',
-function($scope) {
+function($scope)
+{
+	$scope.count1 = 0;
+	$scope.count2 = 0;
 	
+	var count3 = 0;
+	
+	$scope.incrementFunction = function()
+	{
+		$scope.count1 -= 1;
+		$scope.count2 += 1;
+		
+		//You can use non-scope variables in a function called by a click event; it's only in the HTML file that non-scope variables don't exist.
+		count3++;
+	};
+	
+	//Sandbox start
+	
+	//Sandbox end
 }]);
--- app/src/modules/pages/angular-basics/directives/ang-directives-show-if/AngDirectivesShowIfCtrl.js
+++ app/src/modules/pages/angular-basics/directives/ang-directives-show-if/AngDirectivesShowIfCtrl.js
@@ -4,6 +4,13 @@
 'use strict';
 
 angular.module('myApp').controller('AngDirectivesShowIfCtrl', ['$scope',
-function($scope) {
+function($scope)
+{
+	$scope.button1_show = true;
+	$scope.counter = 0;
 	
+	$scope.resetCounter = function()
+	{
+		$scope.counter = 0;
+	};
 }]);
